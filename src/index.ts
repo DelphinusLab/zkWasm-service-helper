dd0e89e8439abe8a7056835600644503060ac57d
yymone <yymone84@hotmail.com>
2023-01-24 09:47:28 +1100

    expose interface

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export {sayHello, sayGoodbye} from './hello';
 
 export { queryImage, queryImages } from './data/image';
 export { loadTasks, addNewWasmImage, addProvingTask, addDeployTask } from './data/task';
+export { Task, ProvingTask, DeployTask } from './Interface/task';
+export { StatusState, QueryParams } from './Interface/status';
+export { DeploymentInfo, Image } from './Interface/image';
