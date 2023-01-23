7d5936cfffc27b48f4829a487cbf384598143839
yymone <yymone84@hotmail.com>
2023-01-24 09:16:12 +1100

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {sayHello, sayGoodbye} from './hello'
+export {sayHello, sayGoodbye} from './hello';
 
 export { queryImage, queryImages } from './data/image';
 export { loadTasks, addNewWasmImage, addProvingTask, addDeployTask } from './data/task';
