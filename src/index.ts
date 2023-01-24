b81c025ea992f78fba458a87d955b032cf640d2a
yymone <yymone84@hotmail.com>
2023-01-24 11:04:24 +1100

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-export {sayHello, sayGoodbye} from './hello';
+import {sayHello, sayGoodbye} from './hello';
 
-export { queryImage, queryImages } from './data/image';
-export { loadTasks, addNewWasmImage, addProvingTask, addDeployTask } from './data/task';
-export { Task, ProvingTask, DeployTask } from './Interface/task';
-export { StatusState, QueryParams } from './Interface/status';
-export { DeploymentInfo, Image } from './Interface/image';
+import { queryImage, queryImages } from './data/image';
+import { loadTasks, addNewWasmImage, addProvingTask, addDeployTask } from './data/task';
+import { Task, ProvingTask, DeployTask } from './Interface/task';
+import { StatusState, QueryParams } from './Interface/status';
+import { DeploymentInfo, Image } from './Interface/image';
+export {
+    sayHello, sayGoodbye, 
+    queryImage, queryImages,
+    loadTasks, addNewWasmImage, addProvingTask, addDeployTask,
+    Task, ProvingTask, DeployTask,
+    StatusState, QueryParams,
+    DeploymentInfo, Image
+};
