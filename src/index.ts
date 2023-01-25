075e7d7a338fff9cfc90aef5868a6544c8d1bb79
yymone <yymone84@hotmail.com>
2023-01-26 00:12:35 +1100

    work version tested

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 
-//import { DeploymentInfo, Image } from "./interface/image";
-//import { VerifyData, QueryParams, StatusState } from "./interface/status";
-//import { Task, ProvingTask, DeployTask } from "./interface/task";
-//import { ZkWasmServiceHelper, ZkWasmServiceTaskHelper, ZkWasmServiceImageHelper } from "./helper/helper";
-
-//export { DeploymentInfo, Image, VerifyData, QueryParams, StatusState, Task, ProvingTask, DeployTask, ZkWasmServiceHelper, ZkWasmServiceTaskHelper, ZkWasmServiceImageHelper };
-
 import axios from "axios";
 import FormData from "form-data";
 
