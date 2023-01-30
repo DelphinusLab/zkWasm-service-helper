7f38c7431f41fc8a312573b5d7c826fccec231ba
yymone <yymone84@hotmail.com>
2023-01-30 13:41:10 +1100

    refine

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -4,46 +4,48 @@ import BN from "bn.js";
 
 import { QueryParams, ProvingTask, DeployTask } from "../interface/interface.js";
 
-export const hexToBNs = (hexString: string): Array<BN> => {
-    let bytes = new Array(hexString.length / 2);
-    for (var i = 0; i < hexString.length; i += 2) {
-        bytes[i] = new BN(hexString.slice(i, i + 2), 16);
-    }
-    return bytes;
-}
 
-export const parseArg = (input: string): Array<BN> | null => {
-    let inputArray = input.split(":");
-    let value = inputArray[0];
-    let type = inputArray[1];
-    let re1 = new RegExp(/^[0-9A-Fa-f]+$/); // hexdecimal
-    let re2 = new RegExp(/^\d+$/); // decimal
-
-    // Check if value is a number
-    if (!(re1.test(value.slice(2)) || re2.test(value))) {
-        console.log("Error: input value is not an interger number");
-        return null;
-    }
-
-    // Convert value byte array
-    if (type == "i64") {
-        let v: BN;
-        if (value.slice(0, 2) == "0x") {
-            v = new BN(value.slice(2), 16);
-        } else {
-            v = new BN(value);
+export class ZkWasmUtil {
+    static hexToBNs(hexString: string): Array<BN> {
+        let bytes = new Array(hexString.length / 2);
+        for (var i = 0; i < hexString.length; i += 2) {
+            bytes[i] = new BN(hexString.slice(i, i + 2), 16);
         }
-        return [v];
-    } else if (type == "bytes" || type == "bytes-packed") {
-        if (value.slice(0, 2) != "0x") {
-            console.log("Error: bytes input need start with 0x");
+        return bytes;
+    }
+    static parseArg(input: string): Array<BN> | null {
+        let inputArray = input.split(":");
+        let value = inputArray[0];
+        let type = inputArray[1];
+        let re1 = new RegExp(/^[0-9A-Fa-f]+$/); // hexdecimal
+        let re2 = new RegExp(/^\d+$/); // decimal
+    
+        // Check if value is a number
+        if (!(re1.test(value.slice(2)) || re2.test(value))) {
+            console.log("Error: input value is not an interger number");
             return null;
         }
-        let bytes = hexToBNs(value.slice(2));
-        return bytes;
-    } else {
-        console.log("Unsupported input data type: %s", type);
-        return null;
+    
+        // Convert value byte array
+        if (type == "i64") {
+            let v: BN;
+            if (value.slice(0, 2) == "0x") {
+                v = new BN(value.slice(2), 16);
+            } else {
+                v = new BN(value);
+            }
+            return [v];
+        } else if (type == "bytes" || type == "bytes-packed") {
+            if (value.slice(0, 2) != "0x") {
+                console.log("Error: bytes input need start with 0x");
+                return null;
+            }
+            let bytes = ZkWasmUtil.hexToBNs(value.slice(2));
+            return bytes;
+        } else {
+            console.log("Unsupported input data type: %s", type);
+            return null;
+        }
     }
 }
 
@@ -169,7 +171,7 @@ export class ZkWasmServiceTaskHelper extends ZkWasmServiceHelper {
         for (var input of inputs) {
             input = input.trim();
             if (input !== "") {
-                if (parseArg(input) != null) {
+                if (ZkWasmUtil.parseArg(input) != null) {
                     parsedInputs.push(input);
                 }
                 else {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 
 import {Task, ProvingTask, DeployTask, QueryParams, VerifyData, StatusState, DeploymentInfo, Image} from "./interface/interface.js";
 
-import {ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, parseArg, hexToBNs} from "./helper/helper.js";
+import {ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, ZkWasmUtil} from "./helper/helper.js";
 
 export {
     Task, ProvingTask, DeployTask, QueryParams, VerifyData, StatusState, DeploymentInfo, Image,
-    ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, parseArg, hexToBNs
+    ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, ZkWasmUtil
 };
