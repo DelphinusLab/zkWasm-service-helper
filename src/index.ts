bba8e5cf049f7fe16a4856c147560e54ae80f277
yymone <yymone84@hotmail.com>
2023-01-31 20:40:22 +1100

    Revert "add js ext"
    This reverts commit c9b9f2aa2febfb54b91525ba42139616c2406a9d.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
-import {Task, ProvingTask, DeployTask, QueryParams, VerifyData, StatusState, DeploymentInfo, Image} from "./interface/interface.js";
+import {Task, ProvingTask, DeployTask, QueryParams, VerifyData, StatusState, DeploymentInfo, Image} from "./interface/interface";
 
-import {ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, ZkWasmUtil} from "./helper/helper.js";
+import {ZkWasmServiceHelper, ZkWasmServiceImageHelper, ZkWasmServiceTaskHelper, ZkWasmUtil} from "./helper/helper";
 
 export {
     Task, ProvingTask, DeployTask, QueryParams, VerifyData, StatusState, DeploymentInfo, Image,
