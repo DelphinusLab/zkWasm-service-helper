2fe20f4f74775333b974cb8086b81262fd1eee2e
rhaoio <richard@rhao.io>
2023-03-24 17:30:35 +1100

    export pagination type

--- dist/mjs/index.js
+++ dist/mjs/index.js
@@ -1,4 +1,4 @@
 import { ZkWasmUtil } from "./helper/util.js";
 import { ZkWasmServiceEndpoint } from "./helper/endpoint.js";
 import { ZkWasmServiceHelper } from "./helper/task.js";
-export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil };
+export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil, };
