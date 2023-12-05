b2965213ea8feb5c8c92b2c41f756915cc808a4d
rhaoio <richard@rhao.io>
2023-12-05 11:55:10 +1100

    remove fields moved to web3subscriber

--- dist/cjs/index.d.ts
+++ dist/cjs/index.d.ts
@@ -2,7 +2,5 @@ import { Task, ProvingParams, DeployParams, QueryParams, VerifyProofParams, Veri
 import { ZkWasmUtil } from "./helper/util.js";
 import { ZkWasmServiceEndpoint } from "./helper/endpoint.js";
 import { ZkWasmServiceHelper } from "./helper/task.js";
-import { DelphinusBaseProvider, DelphinusBrowserProvider, DelphinusProvider, DelphinusReadOnlyProvider, DelphinusSigner, DelphinusWalletProvider, GetBaseProvider } from "./helper/provider.js";
-import { withBrowserProvider, withDelphinusWalletProvider, withReadOnlyProvider, DelphinusContract } from "./helper/client.js";
-export { type DelphinusBaseProvider, DelphinusBrowserProvider, DelphinusProvider, DelphinusReadOnlyProvider, DelphinusSigner, DelphinusWalletProvider, GetBaseProvider, withBrowserProvider, withDelphinusWalletProvider, withReadOnlyProvider, DelphinusContract, ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil, };
+export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil };
 export type { Task, ProvingParams, DeployParams, QueryParams, VerifyProofParams, VerifyData, StatusState, DeploymentInfo, Image, Statistics, AddImageParams, WithSignature, User, UserQueryParams, PaymentParams, TxHistoryQueryParams, TransactionInfo, ResetImageParams, AppConfig, PaginationResult, LogQuery, ModifyImageParams, ChainDetails, };
