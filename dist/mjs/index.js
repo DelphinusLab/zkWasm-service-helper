046dbad97e8b909e72b0cf473b9ea3ad1c1f75ae
Richard Hao <richardhao1995@hotmail.com>
2023-12-07 14:20:36 +0800

    ZKWAS-171 (#38)
    * web3subscriber package
    
    * update
    
    * remove fields moved to web3subscriber
    
    * update
    
    * update
    
    * update package

--- dist/mjs/index.js
+++ dist/mjs/index.js
@@ -1,6 +1,4 @@
 import { ZkWasmUtil } from "./helper/util.js";
 import { ZkWasmServiceEndpoint } from "./helper/endpoint.js";
 import { ZkWasmServiceHelper } from "./helper/task.js";
-import { DelphinusBrowserProvider, DelphinusProvider, DelphinusReadOnlyProvider, DelphinusSigner, DelphinusWalletProvider, GetBaseProvider, } from "./helper/provider.js";
-import { withBrowserProvider, withDelphinusWalletProvider, withReadOnlyProvider, DelphinusContract, } from "./helper/client.js";
-export { DelphinusBrowserProvider, DelphinusProvider, DelphinusReadOnlyProvider, DelphinusSigner, DelphinusWalletProvider, GetBaseProvider, withBrowserProvider, withDelphinusWalletProvider, withReadOnlyProvider, DelphinusContract, ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil, };
+export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil };
--- src/index.ts
+++ src/index.ts
@@ -27,38 +27,8 @@ import {
 import { ZkWasmUtil } from "./helper/util.js";
 import { ZkWasmServiceEndpoint } from "./helper/endpoint.js";
 import { ZkWasmServiceHelper } from "./helper/task.js";
-import {
-  DelphinusBaseProvider,
-  DelphinusBrowserProvider,
-  DelphinusProvider,
-  DelphinusReadOnlyProvider,
-  DelphinusSigner,
-  DelphinusWalletProvider,
-  GetBaseProvider,
-} from "./helper/provider.js";
-import {
-  withBrowserProvider,
-  withDelphinusWalletProvider,
-  withReadOnlyProvider,
-  DelphinusContract,
-} from "./helper/client.js";
 
-export {
-  type DelphinusBaseProvider,
-  DelphinusBrowserProvider,
-  DelphinusProvider,
-  DelphinusReadOnlyProvider,
-  DelphinusSigner,
-  DelphinusWalletProvider,
-  GetBaseProvider,
-  withBrowserProvider,
-  withDelphinusWalletProvider,
-  withReadOnlyProvider,
-  DelphinusContract,
-  ZkWasmServiceEndpoint,
-  ZkWasmServiceHelper,
-  ZkWasmUtil,
-};
+export { ZkWasmServiceEndpoint, ZkWasmServiceHelper, ZkWasmUtil };
 
 export type {
   Task,
