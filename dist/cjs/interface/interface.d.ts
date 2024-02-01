d97ee0729da13f26c63d7d598e6edee9be91be44
rhaoio <richard@rhao.io>
2024-02-01 18:21:57 +1100

    typo

--- dist/cjs/interface/interface.d.ts
+++ dist/cjs/interface/interface.d.ts
@@ -254,7 +254,7 @@ export interface User {
 }
 export type UserInfo = {
     user: User;
-    susbcription: Subscription | null;
+    subscription: Subscription | null;
 };
 export interface TransactionInfo {
     txhash: string;
