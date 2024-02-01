350722a233596ef2eabea483dd2bbf8c6d1a4414
rhaoio <richard@rhao.io>
2024-02-01 18:38:53 +1100

    update names

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -272,6 +272,8 @@ export interface SubscriptionParams {
   subscription_type: SubscriptionType;
   duration: SubscriptionDuration;
   payment_hash: string;
+  price_per_base_duration: string;
+  enabled: boolean;
 }
 
 export type SubscriptionStatus = "Active" | "Expired";
