7f24172f036a3575957e751c015e584fb86783bd
rhaoio <richard@rhao.io>
2024-02-02 18:15:15 +1100

    more interfaces

--- dist/mjs/interface/interface.d.ts
+++ dist/mjs/interface/interface.d.ts
@@ -162,6 +162,7 @@ export interface AppConfig {
     chain_info_list: Array<ChainInfo>;
     latest_server_checksum: Uint8Array;
     deployments: ContractDeployments[];
+    subscription_plans: SubscriptionParams[];
 }
 export interface ContractDeployments {
     chain_id: number;
@@ -219,12 +220,20 @@ type SubscriptionDuration = {
     multiplier: number;
 };
 export interface SubscriptionParams {
-    subscriber_address: string;
     subscription_type: SubscriptionType;
     duration: SubscriptionDuration;
-    payment_hash: string;
+    token_params: TokenParams;
+    token_data: TokenData;
     price_per_base_duration: string;
     enabled: boolean;
+}
+export interface TokenParams {
+    token_address: string;
+    network: string;
+}
+export interface TokenData {
+    decimals: number;
+    symbol: string;
 }
 export interface SubscriptionRequest {
     subscriber_address: string;
