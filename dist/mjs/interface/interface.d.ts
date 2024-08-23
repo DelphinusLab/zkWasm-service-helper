414369bd8500591d2225ef5ad142f098f5796865
rhaoio <richard@rhao.io>
2024-08-23 20:18:24 +1000

    guest static in task type

--- dist/mjs/interface/interface.d.ts
+++ dist/mjs/interface/interface.d.ts
@@ -59,6 +59,7 @@ export interface Task {
     task_fee?: Uint8Array;
     status_message?: string;
     internal_message?: string;
+    guest_statics?: number;
     task_verification_data: TaskVerificationData;
     debug_logs?: string;
     proof_submit_mode?: ProofSubmitMode;
