f6e72e6c86a545f2d181fb7432c50777b8c2a97d
Richard Hao <richardhao1995@hotmail.com>
2024-11-25 12:37:37 +1100

    ZKWAS-433: Update BatchVerifier contract interface (#113)
    * update batch verifier contract interface
    
    * verify fix
    
    * abi
    
    * try old fn
    
    * fix old
    
    * rename
    
    * examples update
    
    * update log

--- dist/cjs/helper/util.d.ts
+++ dist/cjs/helper/util.d.ts
@@ -94,6 +94,7 @@ export declare class ZkWasmUtil {
     static verifyProof(verify_contract: Contract, params: VerifyProofParams): Promise<import("ethers").ContractTransactionResponse>;
     static composeBatchVerifierContract(signer: DelphinusBrowserConnector | DelphinusWalletConnector, verifier_addr: string): import("web3subscriber/src/client.js").DelphinusContract | Promise<import("web3subscriber/src/client.js").DelphinusContract>;
     static verifyBatchedProof(batch_verifier_contract: Contract, params: VerifyBatchProofParams): Promise<import("ethers").ContractTransactionResponse>;
+    static checkVerifiedProof(batch_verifier_contract: Contract, params: VerifyBatchProofParams): Promise<import("ethers").ContractTransactionResponse>;
     static signMessage(message: string, priv_key: string): Promise<string>;
     static ERC20Contract(contractAddress: string, signer: Signer): Contract;
     static bytesToJSONString(data: Uint8Array): string;
