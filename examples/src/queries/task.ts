a007c64a89819d208b9b46435cc3850f966fc708
qozymandias <odowning19@gmail.com>
2025-03-04 18:33:02 +1100

    add example

--- examples/src/queries/task.ts
+++ examples/src/queries/task.ts
@@ -6,10 +6,12 @@ import {
   CompressionType,
   TaskExternalHostTableParams,
   TaskExternalHostTable,
+  ConciseTask,
 } from "zkwasm-service-helper";
 import BN from "bn.js";
 import { ServiceHelper } from "../config";
 import * as fs from "fs";
+
 
 // Provide the query parameters, all fields are optional
 // const args: QueryParams = {
@@ -20,6 +22,10 @@ import * as fs from "fs";
 //   taskstatus: null,
 // };
 
+// Note: `queryTasks` provides task detail such as `aggregate_proof`, `instances`, `batchInstances`, `aux`
+// and `task_fee`. For most queries `queryConciseTasks` will suffice and be more performant. It is 
+// recommended that for general queries, `queryConciseTasks` approach should be used and if task detail 
+// is required, it should be done by querying with the id, see example `queryConciseThenDetail`.
 export async function queryTasks(queryParams: QueryParams) {
   const response: PaginationResult<Task[]> = await ServiceHelper.loadTasks(
     queryParams
@@ -54,6 +60,55 @@ export async function queryTasks(queryParams: QueryParams) {
   console.log("fee:", fee);
 
   return response;
+}
+
+// Provide the query parameters, all fields are optional
+// const args: QueryParams = {
+//   id: taskid!,
+//   md5: null,
+//   user_address: null,
+//   tasktype: null,
+//   taskstatus: null,
+// };
+
+export async function queryConciseTasks(queryParams: QueryParams) {
+  const response: PaginationResult<ConciseTask[]> = await ServiceHelper.loadTaskList(
+    queryParams
+  );
+  const task: ConciseTask = response.data[0];
+  console.log("Task ID", task._id);
+  console.log("Task user address", task.user_address);
+  console.log("Task md5", task.md5);
+  console.log("Task status", task.status);
+  console.log("Task type", task.task_type);
+  console.log("Task submit time", task.submit_time);
+  console.log("Task start time", task.process_started);
+  console.log("Task finish time", task.process_finished);
+  console.log("Task auto submit status", task.auto_submit_status);
+  console.log("Task proof submit mode", task.proof_submit_mode);
+
+  return response;
+}
+
+export async function queryConciseThenDetail(queryParams: QueryParams) {
+  // Query concise task 
+  const concise = await queryConciseTasks(queryParams);
+
+  // Get the id of the task
+  const id = concise.data[0]._id as string;
+  const queryById: QueryParams = {
+    id: id,
+    md5: null,
+    user_address: null,
+    tasktype: null,
+    taskstatus: null,
+  };
+
+
+  // Query task detail with task id
+  const taskDetail = await queryTasks(queryById);
+
+  console.log("Task detail fetched by id", taskDetail.data[0]._id);
 }
 
 // Provide the id of the task which has the desired external host table file.
