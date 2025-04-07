c9075b37e7cfaac78e523492bf45f5d0da51c903
rhaoio <richard@rhao.io>
2025-04-07 18:07:23 +0900

    update statistic field

--- dist/mjs/interface/interface.d.ts
+++ dist/mjs/interface/interface.d.ts
@@ -14,6 +14,8 @@ export interface NodeStatistics {
         successful_tasks: number;
         failed_tasks: number;
         total_tasks: number;
+        last_successful_task_id?: ObjectId;
+        last_successful_task_ts?: string;
         timed_out_count: number;
         last_timed_out: string;
         last_timed_out_task_id?: ObjectId;
